95ee7fe95a40c460fa15390470dc3c63d0a54ff6
Jędrzej Blak <stilo001@gmail.com>
2015-05-30 22:53:34 +0200

    Initial commit

---
+++ src/main/java/pl/jblak/twitmood/main.java
@@ -0,0 +1,12 @@
+package pl.jblak.twitmood;
+
+/**
+ * Created by Jedrek on 30.05.2015.
+ */
+public class main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("ZAØ”£∆ G åL• JAè—!");
+    }
+}
